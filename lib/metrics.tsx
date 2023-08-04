52d29de91eaa6212875046a510a1842bbf90cb85
N8 <nathan.brachotte+github@gmail.com>
2023-08-04 15:46:49 +0200

    feat: 🎸 add prettier

--- lib/metrics.tsx
+++ lib/metrics.tsx
@@ -1,8 +1,8 @@
-import 'server-only';
+import 'server-only'
 
-import { google } from 'googleapis';
-import { queryBuilder } from 'lib/planetscale';
-import { cache } from 'react';
+import { google } from 'googleapis'
+import { queryBuilder } from 'lib/planetscale'
+import { cache } from 'react'
 
 const googleAuth = new google.auth.GoogleAuth({
   credentials: {
@@ -10,46 +10,46 @@ const googleAuth = new google.auth.GoogleAuth({
     private_key: process.env.GOOGLE_PRIVATE_KEY,
   },
   scopes: ['https://www.googleapis.com/auth/youtube.readonly'],
-});
+})
 
 const youtube = google.youtube({
   version: 'v3',
   auth: googleAuth,
-});
+})
 
 export const getBlogViews = cache(async () => {
-  if (!process.env.DATABASE_URL) {
-    return 0;
+  if (!process.env.DATABASE_HOST) {
+    return 0
   }
 
   const data = await queryBuilder
     .selectFrom('views')
     .select(['count'])
-    .execute();
+    .execute()
 
-  return data.reduce((acc, curr) => acc + Number(curr.count), 0);
-});
+  return data.reduce((acc, curr) => acc + Number(curr.count), 0)
+})
 
 export const getViewsCount = cache(async () => {
-  return queryBuilder.selectFrom('views').select(['slug', 'count']).execute();
-});
+  return queryBuilder.selectFrom('views').select(['slug', 'count']).execute()
+})
 
 export const getLeeYouTubeSubs = cache(async () => {
   const response = await youtube.channels.list({
     id: ['UCZMli3czZnd1uoc1ShTouQw'],
     part: ['statistics'],
-  });
+  })
 
-  let channel = response.data.items![0];
-  return Number(channel?.statistics?.subscriberCount);
-});
+  let channel = response.data.items![0]
+  return Number(channel?.statistics?.subscriberCount)
+})
 
 export const getVercelYouTubeSubs = cache(async () => {
   const response = await youtube.channels.list({
     id: ['UCLq8gNoee7oXM7MvTdjyQvA'],
     part: ['statistics'],
-  });
+  })
 
-  let channel = response.data.items![0];
-  return Number(channel?.statistics?.subscriberCount);
-});
+  let channel = response.data.items![0]
+  return Number(channel?.statistics?.subscriberCount)
+})
