16319f53c098ec01ad6f2a4afe3912ff4136dbf4
N8 <nathan.brachotte+github@gmail.com>
2023-08-06 15:57:29 +0200

    fix: 🐛 Fix blog link

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -14,7 +14,7 @@ export default async function BlogPage() {
   console.log({ allBlogs })
   return (
     <section>
-      <h1 className="font-bold text-2xl mb-8 tracking-tighter">read my blog</h1>
+      <h1 className="mb-8 text-2xl font-bold tracking-tighter">read my blog</h1>
       {allBlogs
         .sort((a, b) => {
           if (new Date(a.publishedAt) > new Date(b.publishedAt)) {
@@ -25,11 +25,11 @@ export default async function BlogPage() {
         .map((post) => (
           <Link
             key={post.slug}
-            className="flex flex-col space-y-1 mb-4"
-            href={`/blog/${post.slug}`}
+            className="mb-4 flex flex-col space-y-1"
+            href={post.slug}
           >
-            <div className="w-full flex flex-col">
-              <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
+            <div className="flex w-full flex-col">
+              <p className="tracking-tight text-neutral-900 dark:text-neutral-100">
                 {post.title}
               </p>
               {/* <ViewCounter
