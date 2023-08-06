c3bebcecd032f070dce2e84d0489de3f2e061da7
N8 <nathan.brachotte+github@gmail.com>
2023-08-06 12:14:26 +0200

    refactor: 💡 Force dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -67,12 +67,14 @@ export default function RootLayout({
     <html
       lang="en"
       className={clsx(
-        'text-black bg-white dark:text-white dark:bg-[#111010]',
+        'dark',
+        // 'bg-white text-black dark:bg-slate-800 dark:text-white',
+        'bg-slate-900 text-white',
         graphik.variable,
       )}
     >
-      <body className="antialiased max-w-2xl mb-40 flex flex-col md:flex-row mx-4 mt-8 lg:mx-auto">
-        <main className="flex-auto min-w-0 mt-6 flex flex-col px-2 md:px-0">
+      <body className="mx-4 mb-40 mt-8 flex max-w-2xl flex-col antialiased md:flex-row lg:mx-auto">
+        <main className="mt-6 flex min-w-0 flex-auto flex-col px-2 md:px-0">
           <Sidebar />
           {children}
           <Analytics />
