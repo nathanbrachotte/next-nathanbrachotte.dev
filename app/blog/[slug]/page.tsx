b05a72a46966e5e606ac9d7d43720abf94c4ad34
N8 <nathan.brachotte+github@gmail.com>
2023-11-14 18:40:40 +0100

    refactor: 💡 update cover img + linkedin head fallback

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -55,6 +55,9 @@ export async function generateMetadata({
           {
             url: image,
           },
+          {
+            url: image.replace('cover', 'cover-fallback'),
+          },
         ],
       }),
     },
