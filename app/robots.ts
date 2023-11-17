8aebdb4bc135b7b2bf60c1a3dfcacebca7ed4243
N8 <nathan.brachotte+github@gmail.com>
2023-11-17 16:07:49 +0100

    refactor: 💡 allow all on robots file

--- app/robots.ts
+++ app/robots.ts
@@ -5,6 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: [
       {
         userAgent: '*',
+        allow: ['/'],
       },
     ],
     sitemap: 'https://nathanbrachotte.dev/sitemap.xml',
