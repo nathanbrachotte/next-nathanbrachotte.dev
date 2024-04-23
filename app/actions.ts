124de2c0d0fd39fb37e549cbfe59d92df357ecbf
N8 <nathan.brachotte+github@gmail.com>
2024-04-23 18:24:09 +0200

    try add some logs to revalidate path

--- app/actions.ts
+++ app/actions.ts
@@ -9,10 +9,12 @@ import { kv } from '@vercel/kv'
 export async function increment(slug: string) {
   try {
     const currentViews = await kv.get<number>(`blog_post_views_${slug}`)
-
+    console.log({ currentViews })
     await kv.set(`blog_post_views_${slug}`, (currentViews ?? 0) + 1)
 
     revalidatePath(`/blog/${slug}`)
+    revalidatePath(`/blog`)
+    console.log(`Revalidated: /blog and /blog/${slug}`)
   } catch (error) {
     console.error(error)
   }
