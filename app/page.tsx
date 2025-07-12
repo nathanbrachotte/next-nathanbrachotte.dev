c5cb5e5dc9546bb27986fe1b24b05dcb5013c296
N8 <nathan.brachotte+github@gmail.com>
2025-07-12 23:28:23 +0200

    improve color

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,8 @@ import { Badges } from 'app/components/Badges'
 import { GradientLink } from 'app/components/GradientLink'
 import { NateDescription } from 'app/components/NateDescription'
 import { H1, H2 } from 'app/components/Typography'
-import { ProjectLogo } from 'app/projects/[slug]/ProjectLogo'
-import { ProjectStatus } from 'app/projects/ProjectStatus'
-import clsx from 'clsx'
-import { type Project, allBlogs, allProjects } from 'contentlayer/generated'
-import { SimpleAnalyticsIcon } from 'icons/SimpleAnalytics'
+import { ProjectCard } from 'app/projects/ProjectCard'
+import { allBlogs, allProjects } from 'contentlayer/generated'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
@@ -14,71 +11,6 @@ import { twMerge } from 'tailwind-merge'
 
 import { getViewsCount } from 'lib/metrics'
 import { BlogPostCard } from './blog/page'
-
-const ProjectCard = ({
-  project: { title, summary, image, slug, analytics, status },
-  index,
-}: {
-  project: Project
-  index: number
-}) => {
-  const borderRadiant = [
-    'from-gradient-purple via-transparent to-gradient-pink',
-    'from-gradient-blue via-transparent to-gradient-cyan',
-    'from-gradient-orange via-transparent to-gradient-yellow',
-    'from-gradient-purple via-transparent to-gradient-orange',
-    'from-gradient-green via-transparent to-gradient-turquoise',
-    'from-gradient-pink via-transparent to-gradient-blue',
-    'from-gradient-cyan via-transparent to-gradient-green',
-    'from-gradient-yellow via-transparent to-gradient-purple',
-  ]
-
-  const animatedGradient = [
-    'via-gradient-yellow',
-    'via-gradient-orange',
-    'via-gradient-cyan',
-    'via-gradient-blue',
-    'via-gradient-green',
-    'via-gradient-turquoise',
-    'via-gradient-pink',
-    'via-gradient-purple',
-  ] as const
-
-  return (
-    <Link href={slug}>
-      <div className="group flex h-full cursor-pointer flex-col justify-between transition-all hover:scale-105">
-        <div
-          className={clsx(
-            'relative mx-auto h-full overflow-hidden rounded-lg bg-gradient-to-r p-[1px] transition-all duration-300 ease-in-out',
-            borderRadiant[index % borderRadiant.length],
-          )}
-        >
-          <div
-            className={clsx(
-              'visible absolute -bottom-96 -top-96 left-10 right-10',
-              `bg-gradient-to-r from-transparent group-hover:visible group-hover:animate-spin-surround group-focus:visible ${
-                animatedGradient[index % animatedGradient.length]
-              } to-transparent`,
-            )}
-          />
-          <div className="relative h-full rounded-lg bg-background p-6">
-            <div className="flex flex-row items-center justify-between">
-              <div className="flex items-center gap-2">
-                <ProjectLogo image={image} title={title} />
-                {status ? <ProjectStatus status={status} /> : null}
-              </div>
-              {analytics != null ? (
-                <SimpleAnalyticsIcon className="h-6 w-6" />
-              ) : null}
-            </div>
-            <H2 className="mt-2 text-xl font-bold tracking-tighter">{title}</H2>
-            <p className="font-md text-slate-300">{summary}</p>
-          </div>
-        </div>
-      </div>
-    </Link>
-  )
-}
 
 export default async function Page() {
   const allViews = await getViewsCount()
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,83 +1,11 @@
 import { H1, H2 } from 'app/components/Typography'
-import { ProjectLogo } from 'app/projects/[slug]/ProjectLogo'
-import { ProjectStatus } from 'app/projects/ProjectStatus'
-import clsx from 'clsx'
-import { type Project, allProjects } from 'contentlayer/generated'
-import { SimpleAnalyticsIcon } from 'icons/SimpleAnalytics'
+import { ProjectCard } from 'app/projects/ProjectCard'
+import { allProjects } from 'contentlayer/generated'
 import { Metadata } from 'next'
-import Link from 'next/link'
 
 export const metadata: Metadata = {
   title: 'N8 - Projects',
   description: "Some of my side projects and apps I've worked on",
-}
-
-const ProjectCard = ({
-  project: { title, summary, image, slug, analytics, status },
-  index,
-}: {
-  project: Project
-  index: number
-}) => {
-  const borderRadiant = [
-    'from-gradient-purple via-transparent to-gradient-pink',
-    'from-gradient-blue via-transparent to-gradient-cyan',
-    'from-gradient-orange via-transparent to-gradient-yellow',
-    'from-gradient-purple via-transparent to-gradient-orange',
-    'from-gradient-green via-transparent to-gradient-turquoise',
-    'from-gradient-pink via-transparent to-gradient-blue',
-    'from-gradient-cyan via-transparent to-gradient-green',
-    'from-gradient-yellow via-transparent to-gradient-purple',
-  ]
-
-  const animatedGradient = [
-    'via-gradient-yellow',
-    'via-gradient-orange',
-    'via-gradient-cyan',
-    'via-gradient-blue',
-    'via-gradient-green',
-    'via-gradient-turquoise',
-    'via-gradient-pink',
-    'via-gradient-purple',
-  ] as const
-
-  // TODO: Add touch detection for animation to run on mobile - https://www.npmjs.com/package/react-touch
-  return (
-    <Link href={slug}>
-      <div className="group flex h-full cursor-pointer flex-col justify-between transition-all hover:scale-105">
-        <div
-          className={clsx(
-            'relative mx-auto h-full overflow-hidden rounded-lg bg-gradient-to-r p-[1px] transition-all duration-300 ease-in-out',
-            borderRadiant[index % borderRadiant.length],
-          )}
-        >
-          <div
-            className={clsx(
-              'visible absolute -bottom-96 -top-96 left-10 right-10',
-              `bg-gradient-to-r from-transparent group-hover:visible group-hover:animate-spin-surround group-focus:visible ${
-                animatedGradient[index % animatedGradient.length]
-              } to-transparent`,
-            )}
-          />
-          <div className="relative h-full rounded-lg bg-background p-6">
-            {
-              <div className="flex flex-row items-center justify-between">
-                <div className="flex items-center gap-2">
-                  <ProjectLogo image={image} title={title} />
-                  {status ? <ProjectStatus status={status} /> : null}
-                </div>
-                {analytics != null ? (
-                  <SimpleAnalyticsIcon className="h-6 w-6" />
-                ) : null}
-              </div>
-            }
-            <H2 className="mt-2 text-xl font-bold tracking-tighter">{title}</H2>
-            <p className="font-md text-slate-300">{summary}</p>
-          </div>
-        </div>
-      </div>
-    </Link>
-  )
 }
 
 export default function ProjectsPage() {
